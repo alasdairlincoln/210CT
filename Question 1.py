2354e043089b283df1aa72104f41783481119efd
Alasdair <Alasdair@ALASDAIR-PC>
2016-10-09 16:04:25 +0100

    Updated Question 1 to work in visual studio, added a project file

--- Question 1.py
+++ Question 1.py
@@ -1,39 +1,39 @@
-number1 = false
-number2 = false
-number3 = false
-number4 = false
+number1 = False
+number2 = False
+number3 = False
+number4 = False
 
-while number1 == false:
+while number1 == False:
     try:
-        a = int(input("please enter a number: "))
-        number1 = true
+        a = int(input("Please enter a number: "))
+        number1 = True
     except valueerror:
-        print("thats not a number. please enter a whole number: ")
-        number1 = false
+        print("Thats not a number. Please enter a whole number: ")
+        number1 = False
 
-while number2 == false:
+while number2 == False:
     try:
-        b = int(input("please enter a second number: "))
-        number2 = true
+        b = int(input("Please enter a second number: "))
+        number2 = True
     except valueerror:
-        print("thats not a number. please enter a whole number: ")
-        number2 = false
+        print("Thats not a number. Please enter a whole number: ")
+        number2 = False
 
-while number3 == false:
+while number3 == False:
     try:
-        c = int(input("please enter a third number: "))
-        number3 = true
+        c = int(input("Please enter a third number: "))
+        number3 = True
     except valueerror:
-        print("thats not a number. please enter a whole number: ")
-        number3 = false
+        print("Thats not a number. Please enter a whole number: ")
+        number3 = False
 
-while number4 == false:
+while number4 == False:
     try:
-        d = int(input("please enter a fourth number: "))
-        number4 = true
+        d = int(input("Please enter a fourth number: "))
+        number4 = True
     except valueerror:
-        print("thats not a number. please enter a whole number: ")
-        number4 = false
+        print("Thats not a number. Please enter a whole number: ")
+        number4 = False
 
 fraction1 = a/b
 fraction2 = c/d
