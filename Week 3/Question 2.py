d5e2dc345d30f2d438f5f8a93ac982353ce84901
Alasdair <Alasdair@ALASDAIR-PC>
2016-10-25 16:25:21 +0100

    Started question 2 week 3

---
+++ Week 3/Question 2.py
@@ -0,0 +1,6 @@
+def is_prime(n):
+    if n <= 1:
+        return false #A prime number is a natural number thats greater than 1
+    elif n <= 3:
+        return true #2 and 3 are prime numbers so dont need to be checked
+    elif
